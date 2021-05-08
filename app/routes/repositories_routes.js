979d3af7f01094a31219d0915dadf4836d5c34ea
Paulo Cavalcanti <paulohn27@hotmail.com>
2021-05-08 19:19:57 -0300

    remove unused dependencies and updated repositories_route to make it cleaner

--- app/routes/repositories_routes.js
+++ app/routes/repositories_routes.js
@@ -1,7 +1,8 @@
-const { json } = require("body-parser");
-const { request } = require("express");
-
 const https = require("https");
+
+/**
+ * Options to be used in the requisition to the github API
+ */
 const options = {
   hostname: "api.github.com",
   path:
@@ -12,35 +13,57 @@ const options = {
   },
 };
 
+/**
+ * Receives the response from the github API as a parameter
+ * Returns a JSON formatted in a way to fit the items of a dynamic content block in the blip builder
+ */
+function formatJSON(form) {
+  formatted = form.map((elem) => {
+    let obj = {
+      header: {
+        type: "application/vnd.lime.media-link+json",
+
+        value: {
+          title: elem.name,
+          text: elem.description,
+          type: "image/jpeg",
+          uri: elem.owner.avatar_url,
+        },
+      },
+    };
+    return obj;
+  });
+
+  return formatted;
+}
+
 module.exports = function (app) {
   app.get("/repositories", (req, res) => {
     https.get(options, (resp) => {
       let data = "";
+
+      /**
+       * While receiving chunks of data, keep appending to the variable which will hold the response
+       */
       resp.on("data", (chunk) => {
         data += chunk;
       });
 
+      /**
+       * When done receiving the chunks of data, format the response to an adequate result
+       * In this case, the result is formatted to fit the dynamic content block in the blip builder
+       */
       resp.on("end", () => {
         form = JSON.parse(data);
-        formatted = form.map((elem) => {
-          let obj = {
-            header: {
-              type: "application/vnd.lime.media-link+json",
-              value: {
-                title: elem.name,
-                text: elem.description,
-                type: "image/jpeg",
-                uri: elem.owner.avatar_url,
-              },
-            },
-          };
-          return obj;
-        });
-        console.log(formatted);
+
+        formatted = formatJSON(form);
+
         let content = {
-            "itemType": "application/vnd.lime.document-select+json",
-            "items": formatted
+          itemType: "application/vnd.lime.document-select+json",
+
+          items: formatted,
         };
+
         res.send(content);
       });
 
